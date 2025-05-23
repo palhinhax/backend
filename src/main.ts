406e3409c22307754ee213b27fd75573cbc4eb89
palhinhax <joao.mduart@gmail.com>
2025-05-23 16:18:49 +0100

    fix: add palhinhax.github.io to CORS allowed origins

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173', // URL do seu front-end local
       'https://backend-production-b318.up.railway.app', // URL do seu back-end em produção
+      'https://palhinhax.github.io',
     ], // URL do seu front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     allowedHeaders: 'Content-Type, Accept, Authorization', // Cabeçalhos permitidos
